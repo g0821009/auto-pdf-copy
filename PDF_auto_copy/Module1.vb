a71b8bb5822ca90ede3fd85017d7a5ae9d91e51d
isana <g0821009@gmail.com>
2014-09-09 16:14:22 +0900

    v1.01

--- PDF_auto_copy/Module1.vb
+++ PDF_auto_copy/Module1.vb
@@ -1,4 +1,5 @@
 ﻿'version 1.00 2014.9.9.1103 first Release
+'version 1.01 2014.9.9.1613 各エラー処理追記
 
 Imports System.IO
 
@@ -7,19 +8,21 @@ Module Module1
     Sub Main()
         'Dim sourceDirName As String = "C:\Users\t-uehara\Desktop\test\From"
         Dim sourceDirName As String = "\\192.168.160.105\部品加工作業要領書"
-        Dim destDirName As String = "C:\Users\Public\Desktop\test\To\部品加工作業要領書"
+        Dim destDirName As String = "C:\Users\Public\Documents\部品加工作業要領書"
 
         Dim timeCount As Double = Timer
 
-        Console.WriteLine("部品加工作業要領書 差分ファイルコピー開始")
+        Console.WriteLine(destDirName)
+        Console.WriteLine("差分ファイルコピー開始")
 
         fileCopy(sourceDirName, destDirName)
         Console.WriteLine("部品加工作業要領書コピー終了 実行時間(s):" & Timer() - timeCount)
 
         sourceDirName = "\\192.168.160.105\承認pdfデータ領域"
-        destDirName = "C:\Users\Public\Desktop\test\To\承認pdfデータ領域"
+        destDirName = "C:\Users\Public\Documents\承認pdfデータ領域"
 
-        Console.WriteLine("承認pdfデータ領域 差分ファイルコピー開始")
+        Console.WriteLine(destDirName)
+        Console.WriteLine("差分ファイルコピー開始")
 
         fileCopy(sourceDirName, destDirName)
 
@@ -33,9 +36,15 @@ Module Module1
         If destDirName(destDirName.Length - 1) <> Path.DirectorySeparatorChar Then
             destDirName = destDirName + Path.DirectorySeparatorChar
         End If
-
+        Dim files As String()
         'コピー元のディレクトリにあるファイルをコピー
-        Dim files As String() = Directory.GetFiles(sourceDirName, "*.pdf", SearchOption.TopDirectoryOnly)
+        Try
+            files = Directory.GetFiles(sourceDirName, "*.pdf", SearchOption.TopDirectoryOnly)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Console.WriteLine("ネットに繋がっていません")
+            Environment.Exit(0)
+        End Try
         Console.WriteLine("get file name finished")
         Dim f As String
         Dim i As Long = 0
@@ -47,10 +56,16 @@ Module Module1
             Dim destFileName As String = destDirName + Path.GetFileName(f)
             'コピー先にファイルが存在しない、
             '存在してもコピー元より更新日時が古い時はコピーする
-            If Not File.Exists(destFileName) OrElse File.GetLastWriteTime(destFileName) < File.GetLastWriteTime(f) Then
-                File.Copy(f, destFileName, True)
-                Console.WriteLine("    copy:" & Path.GetFileName(f))
-            End If
+            Try
+                If Not File.Exists(destFileName) OrElse File.GetLastWriteTime(destFileName) < File.GetLastWriteTime(f) Then
+                    File.Copy(f, destFileName, True)
+                    Console.WriteLine("    copy:" & Path.GetFileName(f))
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message)
+                Console.WriteLine("コピーに失敗しました")
+                Environment.Exit(0)
+            End Try
             i = i + 1
             If Timer - tmpTime > 2 Then
                 Console.Write(String.Format("{0, 4:p} ", i / maxDim))
